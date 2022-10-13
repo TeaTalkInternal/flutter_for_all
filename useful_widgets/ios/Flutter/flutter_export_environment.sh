8019df1488b45a4c100f37faa6c161eef7c42c52
kevinsaldanhacompass <vishalkevin11@gmail.com>
2022-10-13 20:37:36 +0530

    Updated folder name from new_widgets to useful_widgets

---
+++ useful_widgets/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/kevin.saldanha/flutter_development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/kevin.saldanha/Documents/teatalk_workspace/flutter_for_all/new_widgets"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/kevin.saldanha/Documents/teatalk_workspace/flutter_for_all/new_widgets/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/kevin.saldanha/Documents/teatalk_workspace/flutter_for_all/new_widgets/.dart_tool/package_config.json"
