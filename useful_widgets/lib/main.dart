589930b83aac9e040c9ec08fb9d90db1b559955f
kevinsaldanhacompass <vishalkevin11@gmail.com>
2022-10-15 11:40:24 +0530

    demo ListShimmerPage through main

--- useful_widgets/lib/main.dart
+++ useful_widgets/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:new_widgets/widgets/demo_customized_shimmer_for_list.dart';
 import 'package:new_widgets/widgets/demo_navigation_rail.dart';
 
 import 'widgets/demo_error_widget.dart';
@@ -30,7 +31,17 @@ class MyApp extends StatelessWidget {
       // home: const DemoExactAssetsImages(),
       // home: const DemoStatefulBuilder(),
       // home: const DemoFractionallySizedBox(),
-      home: const DemoNavigationRail(),
+      // home: const DemoNavigationRail(),
+      home: const ListShimmerPage(
+          containerHeight: 60.0,
+          bottomMargin: 5.0,
+          leftMargin: 10.0,
+          rightMargin: 10.0,
+          topMargin: 10.0,
+          count: 10,
+          borderRadius: 4.0,
+          shinkWap: false,
+          physics: false),
       // home: const DemoImageFilter(),
       // home: const DemoListWheelScrollView(),
     );
